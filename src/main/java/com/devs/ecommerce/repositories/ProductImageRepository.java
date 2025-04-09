00a318d8212ae4ad22dfb9e07c65e17449ee686c
Monder AISSOU <badiaissou02@gmail.com>
2025-04-09 23:51:12 +0100

    Product can have multiple images

---
+++ src/main/java/com/devs/ecommerce/repositories/ProductImageRepository.java
@@ -0,0 +1,10 @@
+package com.devs.ecommerce.repositories;
+
+/*import com.devs.ecommerce.model.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+}
+*/
